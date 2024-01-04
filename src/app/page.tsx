eaec3d0178f557d0d2c37c849d3967df625b6032
Vinícius Gutierrez <srgutyerrez@gmail.com>
2024-01-03 23:10:53 -0300

    chore: use sidebar component and useDocument hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { Heading } from '@vyrnnstudios/next-template/components';
+import { Heading } from '@vyrnnstudios/documentation/components';
+
+import { Sidebar } from '@vyrnnstudios/documentation/components/Sidebar';
+
+import { useDocumentation } from '@vyrnnstudios/documentation/hooks';
 
 export default function () {
+  const { fetchSettingsFromDocumentation } = useDocumentation();
+
   return (
     <div>
-      <Heading>Welcome to Vyrnn Studios docs!</Heading>
+      <Sidebar settings={fetchSettingsFromDocumentation()} />
+      <div>
+        <Heading>Welcome to Vyrnn Studios docs!</Heading>
+      </div>
     </div>
   );
 }
