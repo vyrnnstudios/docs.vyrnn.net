7d34071a7f5bbb5d87d9c3a5c8ead85d7b123f57
Vinícius Gutierrez <srgutyerrez@gmail.com>
2024-01-05 09:55:42 -0300

    feat: create icon component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from '@vyrnnstudios/documentation/components/Heading';
 
+export * from '@vyrnnstudios/documentation/components/Icon';
+
 export * from '@vyrnnstudios/documentation/components/Sidebar';
